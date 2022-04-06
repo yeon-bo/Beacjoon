775b75c5b792826df7bfa55e079cc01004c8c1a9
yeon-bo <80714114+yeon-bo@users.noreply.github.com>
2022-04-06 20:47:02 +0900

    Create 10430.js

---
+++ solutions/10430.js
@@ -0,0 +1,12 @@
+let fs = require('fs');
+let input = fs.readFileSync('./dev/stdin').toString().split('\n');
+let line = input[0].split(' ');
+
+let a = parseInt(line[0]);
+let b = parseInt(line[1]);
+let c = parseInt(line[2]);
+
+console.log(parseInt((a+b)%c))
+console.log(parseInt(((a%c) + (b%c))%c))
+console.log(parseInt((a*b)%c))
+console.log(parseInt(((a%c) * (b%c))%c))
