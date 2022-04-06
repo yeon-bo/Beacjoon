187231b076b9063988d132f8533c6e65d030c580
yeon-bo <80714114+yeon-bo@users.noreply.github.com>
2022-04-06 20:49:01 +0900

    Create 10950.js

---
+++ solutions/10950.js
@@ -0,0 +1,8 @@
+let fs = require('fs');
+let input = fs.readFileSync('./dev/stdin').toString().split('\n');
+
+let n = Number(input[0]);
+for (let i = 1; i <= n; i++) {
+  let [a, b] = input[i].split(' ').map(x => Number(x));
+  console.log(a + b);
+}
