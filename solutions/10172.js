8367196d7f5fc9892d400181934999582332bf04
yeon-bo <80714114+yeon-bo@users.noreply.github.com>
2022-04-06 20:46:13 +0900

    Create 10172.js

---
+++ solutions/10172.js
@@ -0,0 +1,8 @@
+let fs = require('fs');
+let input = fs.readFileSync('./dev/stdin').toString().split('\n');
+
+console.log("|\\_/|")
+console.log("|q p|   /}")
+console.log("( 0 )\"\"\"\\")
+console.log("|\"^\"`    |")
+console.log("||_/=\\\\__|")
