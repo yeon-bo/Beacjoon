2a1349d2995e2d093844c78940d2581179789a98
yeon-bo <80714114+yeon-bo@users.noreply.github.com>
2022-04-06 23:07:34 +0900

    Create 2606.js

---
+++ solutions/2606.js
@@ -0,0 +1,29 @@
+let input = require('fs').readFileSync('/dev/stdin').toString().split('\n');
+
+let n = Number(input[0]); // 노드의 개수(N)
+let m = Number(input[1]); // 간선의 개수(M)
+
+let graph = []; // 전체 그래프
+for (let i = 0; i <= n; i++) graph.push([]);
+// 각 노트에 대한 방문 여부
+let visited = new Array(n + 1).fill(false);
+let result = 0;
+
+for (let i = 2; i < m + 2; i++) {
+  let [x, y] = input[i].split(' ').map(Number);
+  graph[x].push(y);
+  graph[y].push(x);
+}
+function dfs(x) {
+  visited[x] = true; // 방문 처리
+  result++; // 카운트
+  for (let i of graph[x]){
+    // 인접한 노드 중에서 방문하지 않은 노드 방문
+    if (visited[i] == false ) {
+      dfs(i);
+    }
+  }
+}
+
+dfs(1);
+console.log(result - 1);
