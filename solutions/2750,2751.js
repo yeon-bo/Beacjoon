1bd74d217dadca30ac27b5a00ffeee118f833195
yeon-bo <80714114+yeon-bo@users.noreply.github.com>
2022-04-10 04:32:42 +0900

    Update 2750,2751.js

--- solutions/2750,2751.js
+++ solutions/2750,2751.js
@@ -14,16 +14,15 @@ for (let i of arr ) {
 
 // 방식은 똑같으나 파일을 읽어오는것이 
 
-let fs = require('fs');
-let input = fs.readFileSync('/dev/stdin').toString().split('\n');
-
 let n = Number(input[0]);
 let arr = [];
 for (let i = 1; i <= n; i++) {
-  arr.push(input[i])
+  arr.push(Number(input[i]))
 }
 arr.sort((a, b) => a - b);
 
-for (let i of arr ) {
- console.log(i); 
+let result = '';
+for (let i = 0; i < n; i++ ) {
+ result += arr[i] + '\n';
 }
+console.log(result)
