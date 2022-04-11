0037e75d2dccb00f7895c25d1ce3e815bbcfb0c8
yeon-bo <80714114+yeon-bo@users.noreply.github.com>
2022-04-11 23:18:10 +0900

    Create 1931.js

---
+++ solutions/1931.js
@@ -0,0 +1,26 @@
+let fs = require('fs');
+let input = fs.readFileSync('./input.txt').toString().split('\n');
+// let input = fs.readFileSync('./dev/stdin').toString().split('\n');
+
+let n = Number(input[0]);
+let arr = [];
+for (let i = 1; i <= n; i++) {
+  arr.push(input[i].split(' '));
+}
+arr.sort((a, b) => {
+  if (a[1] != b[1]) return a[1] - b[1];
+  else return a[0] - b[0];
+});
+
+console.log(arr); 
+let count = 1;
+let cnt = 0;
+for (let i = 1; i < n; i++) {
+  if (arr[cnt][1] <= arr[i][0]) {
+  cnt = i;
+  count++;
+}}
+
+console.log(count);
+
+// 해법은 맞는데 답이 이상한 것 같다. 나중에 확인 
